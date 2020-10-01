d89d468ca41c034b34f75558a7fed3849aed11cb
OlegBirykov <p_petro75@mail.ru>
2020-10-01 23:29:22 +0300

    Добавлены тесты Puppeteer, задание выполнено

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,3 +1,5 @@
+/* eslint no-underscore-dangle: 0 */
+
 import puppetteer from 'puppeteer';
 import { fork } from 'child_process';
 
@@ -8,6 +10,22 @@ describe('Product List Editor', () => {
   let page = null;
   let server = null;
   const baseUrl = 'http://localhost:9000';
+
+  let priceListWidget = null;
+  let addButton = null;
+  let productList = null;
+
+  let editForm = null;
+  let nameInput = null;
+  let priceInput = null;
+  let saveButton = null;
+  let cancelSaveButton = null;
+
+  let deleteForm = null;
+  let deleteButton = null;
+  let cancelDeleteButton = null;
+
+  let errorTooltips = null;
 
   beforeAll(async () => {
     server = fork(`${__dirname}/e2e.server.js`);
@@ -26,6 +44,23 @@ describe('Product List Editor', () => {
       // devtools: true, // show devTools
     });
     page = await browser.newPage();
+    await page.goto(baseUrl);
+
+    priceListWidget = await page.$('[data-widget=price-list-form]');
+    addButton = await priceListWidget.$('[data-id=command-create]');
+    productList = await priceListWidget.$('[data-id=product-list]');
+
+    editForm = await page.$('[data-widget=edit-form]');
+    nameInput = await editForm.$('[data-id=name]');
+    priceInput = await editForm.$('[data-id=price]');
+    saveButton = await editForm.$('[data-id=save]');
+    cancelSaveButton = await editForm.$('[data-id=cancel]');
+
+    deleteForm = await page.$('[data-widget=delete-form]');
+    deleteButton = await deleteForm.$('[data-id=delete]');
+    cancelDeleteButton = await deleteForm.$('[data-id=cancel]');
+
+    errorTooltips = await page.$$('[data-widget=tooltip]');
   });
 
   afterAll(async () => {
@@ -33,32 +68,79 @@ describe('Product List Editor', () => {
     server.kill();
   });
 
-  test('should add do something', async () => {
-    await page.goto(baseUrl);
-  });
-/*
-  test('should add .valid class for valid number', async () => {
-    const widget = await page.$('[data-widget=card-validator-widget]');
-    const input = await widget.$('[data-id=input]');
-    const result = await widget.$('[data-id=result]');
+  test('the product should be added', async () => {
+    await addButton.click();
+    let status = await editForm.getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(true);
 
-    await input.type('3540123456789016');
-    await input.press('Enter');
-    const status = await result.getProperty('className');
-    // eslint-disable-next-line no-underscore-dangle
-    expect(status._remoteObject.value).toBe('result valid');
+    await cancelSaveButton.click();
+    status = await editForm.getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(false);
+
+    await addButton.click();
+    await saveButton.click();
+    status = await errorTooltips[0].getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(true);
+    status = await errorTooltips[1].getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(false);
+
+    await nameInput.type('Пылесос');
+    await saveButton.click();
+    status = await errorTooltips[0].getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(false);
+    status = await errorTooltips[1].getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(true);
+
+    await priceInput.type('8 500');
+    await saveButton.click();
+    status = await errorTooltips[0].getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(false);
+    status = await errorTooltips[1].getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(false);
+    const rows = await productList.$$('tr');
+    const cols = await rows[0].$$('td');
+    status = await cols[0].getProperty('innerText');
+    expect(status._remoteObject.value).toBe('Пылесос');
+    status = await cols[1].getProperty('innerText');
+    expect(status._remoteObject.value).toBe('8 500');
   });
 
-  test('should not add .valid class for invalid number', async () => {
-    const widget = await page.$('[data-widget=card-validator-widget]');
-    const input = await widget.$('[data-id=input]');
-    const result = await widget.$('[data-id=result]');
+  test('the product should be updated', async () => {
+    const editButtons = await productList.$$('[data-id=command-edit]');
 
-    await input.type('3540123456789015');
-    await input.press('Enter');
-    const status = await result.getProperty('className');
-    // eslint-disable-next-line no-underscore-dangle
-    expect(status._remoteObject.value).toBe('result');
+    await editButtons[0].click();
+    let status = await editForm.getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(true);
+
+    await priceInput.press('Backspace');
+    await priceInput.press('Backspace');
+    await priceInput.press('Backspace');
+    await priceInput.type('200');
+    await saveButton.click();
+    const rows = await productList.$$('tr');
+    const cols = await rows[0].$$('td');
+    status = await cols[0].getProperty('innerText');
+    expect(status._remoteObject.value).toBe('Пылесос');
+    status = await cols[1].getProperty('innerText');
+    expect(status._remoteObject.value).toBe('8 200');
   });
-  */
+
+  test('the product should be deleted', async () => {
+    const delButtons = await productList.$$('[data-id=command-delete]');
+
+    await delButtons[0].click();
+    let status = await deleteForm.getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(true);
+
+    await cancelDeleteButton.click();
+    status = await deleteForm.getProperty('className');
+    expect(status._remoteObject.value.includes('active')).toBe(false);
+
+    let rows = await productList.$$('tr');
+    expect(rows.length).toBe(1);
+    await delButtons[0].click();
+    await deleteButton.click();
+    rows = await productList.$$('tr');
+    expect(rows.length).toBe(0);
+  });
 });
